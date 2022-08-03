4a0975e5f65bbb96783f3803dbfcf35d11b4ec5f
Jordy Torres <jordy.w.torres@unl.edu.ec>
2022-08-03 07:51:14 -0500

    Jordy Torres

---
+++ TrabajoFinalCliclo3/src/modelo/enums/estadoMatricula.java
@@ -0,0 +1,15 @@
+package modelo.enums;
+
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+
+/**
+ *
+ * @author Jordy
+ */
+public enum estadoMatricula {
+    aprobado,rechazado;
+}
