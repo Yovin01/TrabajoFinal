e8a30839f0b10589f49bfca0b5f1f5f1bc884df0
Jordy Torres <jordy.w.torres@unl.edu.ec>
2022-08-04 16:15:41 -0500

    Actualizacion "Jordy Torres"

---
+++ TrabajoFinalCliclo3/src/controlador/conexion/SQLclass.java
@@ -0,0 +1,35 @@
+package controlador.conexion;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+/**
+ *
+ * @author jordy
+ */
+public class SQLclass {
+   private static final String user = "SUNKIDS";
+    private static final String password = "sunkids";
+    
+   public static Connection conecction;
+public SQLclass() {
+    }
+    public static Connection getConecction() {
+        if(conecction == null)
+            conn();
+        return conecction;
+    }    
+       
+     public static Connection conn(){
+        try {
+            Class.forName("oracle.jdbc.driver.OracleDriver");
+            DriverManager.registerDriver(new oracle.jdbc.driver.OracleDriver());
+            System.out.println("Conectando a la base de datos.....");
+          Connection connection= DriverManager.getConnection("jdbc:oracle:thin:@localhost:1521:XE", user,password);  
+            System.out.println("CONEXION EXITOSA");
+            return connection;
+        } catch (Exception e) {
+            System.out.println("Fallo de conexion error!: "+e);
+            return null;
+        }
+    }
+}
